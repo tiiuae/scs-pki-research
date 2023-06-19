32a987150a57e4ffcba8bac8dc3fcbdb414abf31
Aleksandr Tserepov-Savolainen <aleksandr.tserepov-savolainen@unikie.com>
2023-06-19 21:00:11 +0300

    Modify sign and verify scripts
    Signed-off-by: Aleksandr Tserepov-Savolainen <aleksandr.tserepov-savolainen@unikie.com>

--- yubikey/sign.sh
+++ yubikey/sign.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-sha256sum /myshare/$1 > test.txt
-awk '{print $1}' test.txt > digest.txt
+#sha256sum /myshare/$1 > test.txt
+#awk '{print $1}' test.txt > digest.txt
+echo $1 > digest.txt
 xxd -r -p digest.txt digest.bin
-pkcs11-tool --verbose --sign --id 02 -i digest.bin --output-file /myshare/data.sig --pin 81728172
+
+pkcs11-tool --sign --id 02 -i digest.bin --output-file data.sig --pin 81728172 2>1 > /dev/null
+base64 data.sig
--- yubikey/verify.sh
+++ yubikey/verify.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
-sha256sum /myshare/$1 > test.txt
-awk '{print $1}' test.txt > digest.txt
+#sha256sum /myshare/$1 > test.txt
+#awk '{print $1}' test.txt > digest.txt
+# 1 - hash
+# 2 - signature in b64 format
+
+echo $1 > digest.txt
+echo $2 > data.sig
+cat data.sig
+base64 -d data.sig > signature.bin
+
 xxd -r -p digest.txt digest.bin
-pkcs11-tool --verify --id 02 --input-file digest.bin --signature-file /myshare/data.sig --pin 81728172
+pkcs11-tool --verify --id 02 --input-file digest.bin --signature-file signature.bin --pin 81728172
